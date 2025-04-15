826f5bcc33946f73598f63ce2d97c9f841222f4d
amank787 <akumar048@rku.ac.in>
2025-04-15 12:08:38 +0530

    minor changes

--- Milkify/Milkify/milkify-frontend/src/pages/ForgotPassword.jsx
+++ Milkify/Milkify/milkify-frontend/src/pages/ForgotPassword.jsx
@@ -15,7 +15,8 @@ const ForgotPassword = () => {
       navigate("/save-password");  // This should take you to /save-password
     }, 2000);  // Redirect after 2 seconds
   };
-//remove the console.log(data) line if you don't need it
+  // This function simulates sending a password reset link to the user's email.
+
   return (
     <div className="forgot-password-container">
       <div className="forgot-password-box">
